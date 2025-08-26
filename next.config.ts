64cb9d519d26c5defb36a0521648d78e4dd4bb9d
Adil Ajdaa <a.ajdaa@outlook.com>
2025-08-26 16:59:44 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/portfolio',
+  assetPrefix: '/portfolio/', // Add this line
 };
 
 export default nextConfig;
