f599324231ca3f5b2d07da613618cdb4c90e4cb9
Adil Ajdaa <a.ajdaa@outlook.com>
2025-08-26 18:53:43 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.IS_GH_PAGES_DEPLOY === 'true' ? '/portfolio' : '',
   images: {
     unoptimized: true,
   },
