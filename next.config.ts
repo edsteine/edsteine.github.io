7bc31ca996a2382abe6ba1959cf195ba4fe7bc0b
Adil Ajdaa <a.ajdaa@outlook.com>
2025-08-26 17:43:13 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '',
-  assetPrefix: '/',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   images: {
     unoptimized: true,
   },
